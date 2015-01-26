9b08f18bf5475b11b69349e6cbb9e34f58786714
acreations@aeris <aaron.wong.home@gmail.com>
2015-01-26 22:53:25 +0100

    Added angular

--- static/js/config.js
+++ static/js/config.js
@@ -3,16 +3,22 @@ require.config({
   baseUrl: "/static/js",
 
   paths: {
+    "angular": "//ajax.googleapis.com/ajax/libs/angularjs/1.3.10/angular.min",
     "bootstrap": "//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min",
     "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min",
+    "tween-max": "//cdnjs.cloudflare.com/ajax/libs/gsap/1.15.1/TweenMax.min",
   },
 
   shim: {
+    'angular': {
+      exports: "angular"
+    },
     'bootstrap': {
       deps: ["jquery"]
     }
   }
 });
 
-require(['bootstrap'], function () {
+require(['angular', 'app', 'bootstrap'], function (ng, app) {
+  ng.bootstrap(document, [app.name]);
 });
